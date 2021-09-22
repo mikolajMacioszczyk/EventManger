286cf7e29ec273b13d9439c1dd2531fb07641934
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-09-22 21:22:59 +0200

    create offer service

--- Frontend/event-manager/src/app/modules/offer-list/components/offer-box/offer-box.component.ts
+++ Frontend/event-manager/src/app/modules/offer-list/components/offer-box/offer-box.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-offer-box',
   templateUrl: './offer-box.component.html',
   styleUrls: ['./offer-box.component.scss']
 })
-export class OfferBoxComponent implements OnInit {
+export class OfferBoxComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
